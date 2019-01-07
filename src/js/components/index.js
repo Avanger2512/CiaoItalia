3d3381c91c0c3cabf88fe995fadfd33bfdac7b6d
Denis Babiichuk <den2512@ukr.net>
2019-01-08 01:43:04 +0200

    Add responsive for 1280 + 640

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -26,3 +26,94 @@ import './goto'
 //
 //   countdownNumberEl.textContent = countdown;
 // }, 1000);
+
+
+
+// form validation
+	// (function() {
+	// 	var popup  = $('.js-popup'),
+	// 		thanks = $('.js-popup-thanks'),
+	// 		body   = $('body');
+	// 	// welcome
+	// 	$.validate({
+	// 		form: '#callback',
+	// 		onSuccess: function() {
+	// 			post_data = {
+	// 				'name': $('#callback input[name=name]').val(),
+	// 				'tel': $('#callback input[name=tel]').val()
+	// 			};
+	// 			window.piper('http://javago.in.ua/pipeform/forest_callback.php', $("#callback").serialize());
+	// 			// Ajax post data to server
+	// 			$.post('send.php', post_data, function(response) {
+	// 				if (response.type == 'error') {
+	// 					console.log('error');
+	// 				}
+	// 				else {
+	// 					// reset values in all input fields
+	// 					popup.fadeOut();
+	// 					thanks.fadeIn('fast');
+	// 					$('#callback').get(0).reset();
+	// 					setTimeout(function() {
+	// 						thanks.fadeOut('fast');
+	// 						body.css('overflow', 'auto');
+	// 					}, 2500);
+	// 					dataLayer.push({'event': '01'});
+	// 				}
+	// 			}, 'json');
+	// 			return false;
+	// 		}
+	// 	});
+	// 	// footer
+	// 	$.validate({
+	// 		form : '#form',
+	// 		onSuccess: function() {
+	// 			post_data = {
+	// 				'name': $('#form input[name=name]').val(),
+	// 				'email': $('#form input[name=email]').val(),
+	// 				'tel': $('#form input[name=tel]').val(),
+	// 				'comment': $('#form textarea[name=comment]').val()
+	// 			};
+	// 			window.piper('http://javago.in.ua/pipeform/forest_questions.php', $("#form").serialize());
+	// 			$.post('send.php', post_data, function(response) {
+	// 				if (response.type == 'error') {}
+	// 				else {
+	// 					thanks.fadeIn('fast');
+  //
+	// 					$('#form').get(0).reset();
+	// 					setTimeout(function() {
+	// 						thanks.fadeOut('fast');
+	// 						body.css('overflow', 'auto');
+	// 					}, 2500);
+	// 					dataLayer.push({'event': '02'});
+	// 				}
+	// 			}, 'json');
+	// 			return false;
+	// 		}
+	// 	});
+	// 	$.validate({
+	// 		form : '#transfer',
+	// 		onSuccess: function() {
+	// 			post_data = {
+	// 				'name': $('#transfer input[name=name]').val(),
+	// 				'email': $('#transfer input[name=email]').val(),
+	// 				'tel': $('#transfer input[name=tel]').val(),
+	// 				'comment': $('#transfer textarea[name=comment]').val()
+	// 			};
+	// 			window.piper('http://javago.in.ua/pipeform/forest_transfer.php', $("#transfer").serialize());
+	// 			$.post('send.php', post_data, function(response) {
+	// 				if (response.type == 'error') {}
+	// 				else {
+	// 					popup.fadeOut();
+	// 					thanks.fadeIn('fast');
+	// 					$('#transfer').get(0).reset();
+	// 					setTimeout(function() {
+	// 						thanks.fadeOut('fast');
+	// 						body.css('overflow', 'auto');
+	// 					}, 2500);
+	// 					dataLayer.push({'event': '03'});
+	// 				}
+	// 			}, 'json');
+	// 			return false;
+	// 		}
+	// 	});
+	// }());
--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -15,7 +15,23 @@ let options = {
 let optionsTeam = {
   slidesToShow: 5,
   slidesToScroll: 1,
-  arrows: false
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+  ]
 }
 
 citySlider.slick(options);
