4600b1ffe3a4d007e7ed956a61222be0752046ce
Denis Babiichuk <den2512@ukr.net>
2019-01-09 17:32:59 +0200

    Add pixel perfect 1920

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,30 @@
+import Inputmask from "inputmask";
+
+// components
 import './components/index';
+
+
+
+let selector = document.querySelectorAll(".js-mask-tel");
+
+let im = new Inputmask({"mask": "+3 8(999)999 99 99"});
+im.mask(selector);
+
+
+
+$(window).on('scroll', function() {
+  let scrollTop = $(this).scrollTop();
+  checkScrollPosition(scrollTop);
+});
+
+function checkScrollPosition(scroll) {
+  if (scroll >= 40) {
+    $('.js-header').addClass('is-scroll');
+  } else {
+    $('.js-header').removeClass('is-scroll');
+  }
+}
+
+$(document).ready(function(){
+  checkScrollPosition($(window).scrollTop());
+});
